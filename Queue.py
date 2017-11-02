1c3c61c1a702cb8efddb03724ee086e57770fcaa
gxsolo <33292833+gxsolo@users.noreply.github.com>
2017-11-02 17:36:12 -0200

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -3,10 +3,10 @@ class Queue:
     def __init__(self):
         self.queue = []
         self.len_queue = 0
-    def add (self,conteudo):
+    def push (self,conteudo):
         self.queue.append(conteudo)
         self.len_queue += 1
-    def remove (self):
+    def pop (self):
         if not self.empty():
             self.queue.pop(0)
             self.len_queue -= 1
@@ -18,7 +18,7 @@ class Queue:
         if not self.empty():
             return self.queue[0]
         return None
-    def rear (self):
+    def back (self):
         if self.len_queue != 0:
             return self.queue[-1]
         return None
